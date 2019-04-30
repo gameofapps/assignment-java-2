2fdd98a379112fc749a49c118ab9742ea0631ae6
Elson <eremisty@gmail.com>
2019-04-30 06:54:08 -0400

    Format changes

--- Challenge 1/src/Q2.java
+++ Challenge 1/src/Q2.java
@@ -1,14 +1,14 @@
 public class Q2 {
-	
-	public static boolean isPalindrome(String word) {
-		//create array of letters from input string
-		char[]letters = word.toCharArray();
-		
-		//check if word is palindrome
-		for (int i = 0; i < letters.length / 2; i++)					//loop iterates towards center of word
-			if (letters[i] != letters[letters.length -1 -i])			//compare inital and ending elements of array
-				return false;			
-		return true;
-	}
-	
+
+    public static boolean isPalindrome(String word) {
+        //create array of letters from input string
+        char[] letters = word.toCharArray();
+
+        //check if word is palindrome
+        for (int i = 0; i < letters.length / 2; i++) //loop iterates towards center of word
+            if (letters[i] != letters[letters.length - 1 - i]) //compare inital and ending elements of array
+                return false;
+        return true;
+    }
+
 }
