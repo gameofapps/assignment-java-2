3ea550a97edaf7a02b4c73df207865b41a9415aa
GerryZhang7 <Gerryzhang123@gmail.com>
2019-05-13 21:03:39 -0700

    Completed question 2

---
+++ Q2.java
@@ -0,0 +1,43 @@
+package q2;
+
+/**
+ *
+ * @author Gerry
+ */
+public class Q2 {
+
+    public static boolean isPalindrome(String testString) {
+      // Find length of the string and declare a counter variable
+        int stringLength = testString.length(); 
+        int count = 0;
+        
+        // For loop checks through the first and last element of the string, then it moves up one digit and the last digit moves down one 
+        // When the length of the string is odd, integer division floors the value. 
+        // We do this as we do not need to check the middle element for an odd length string
+        for (int i = 0; i < (stringLength / 2); ++i) {
+                if (testString.charAt(0 + i) == testString.charAt(stringLength - i - 1)) {
+                    ++count;
+                }
+                else {
+                    System.out.println("False");
+                    return false;
+                }
+            }
+        // Count keeps track of the number of matching characters, if it equals stringLength/2, then it is a palindrome.
+            if (count == (stringLength / 2) ){
+                System.out.println("True");
+                return true;
+            }
+        return false;
+    }
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String test = "radar";
+        
+        isPalindrome(test);
+        
+    }
+    
+}
