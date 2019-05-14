5e29847ad9c72a9755b980463cb33405701c61bf
GerryZhang7 <Gerryzhang123@gmail.com>
2019-05-13 18:16:24 -0700

    Created android application that shows images

---
+++ Challenge 2/app/src/main/java/com/example/gerry/gameofappschallenge/blackHole.java
@@ -0,0 +1,13 @@
+package com.example.gerry.gameofappschallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class blackHole extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_black_hole);
+    }
+}
---
+++ Challenge 2/app/src/main/java/com/example/gerry/gameofappschallenge/superNova.java
@@ -0,0 +1,13 @@
+package com.example.gerry.gameofappschallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class superNova extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_super_nova);
+    }
+}
