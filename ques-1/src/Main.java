e219679c36e6819f286b029dbbc2c1170c894cca
shashwatregmi <shashwat.regmi@icloud.com>
2020-06-11 10:53:48 -0700

    Update to ques-1 solution

--- ques-1/src/Main.java
+++ ques-1/src/Main.java
@@ -4,24 +4,24 @@ import java.util.HashMap;
 public class Main {
 
     private static void findPairs(int[] testArray, int targetSum) {
-        HashMap<Integer, Integer> tempList = new HashMap<>();
+        ArrayList<Integer> tempList = new ArrayList<>();
 
         for (int i = 0; i < testArray.length; i++){
             int searchValue = targetSum - testArray[i];
 
-            if (tempList.containsKey(searchValue)){
+            if (tempList.contains(searchValue)){
                 System.out.println("(" + testArray[i] + ", " + searchValue + ")");
-                tempList.remove(searchValue);
+                tempList.remove(Integer.valueOf(searchValue));
             } else {
-                tempList.put(testArray[i], i);
+                tempList.add(testArray[i]);
             }
         }
     }
 
 
     public static void main (String args[]){
-//        int[] testArray = {2, 4, 5, 1, 3, 5, 4};
-//        int targetSum = 6;
+//        int[] testArray = {8, 2, 2, 2, 2, 4, 5, 0};
+//        int targetSum = 4;
 //        findPairs(testArray, targetSum);
     }
 }
